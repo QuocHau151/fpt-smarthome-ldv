26d354d1d88b4d833dcee8111d99fe7980c96f9b
Quoc Hau <tranlequochau.blc@gmail.com>
2024-03-08 11:21:16 +0700

    8/3 2

--- actions/fetchProduct.ts
+++ actions/fetchProduct.ts
@@ -2,11 +2,13 @@
 
 import { formatToString } from "@/hook/useCurrency";
 import { db } from "@/lib/db";
-
+type ProductType = {
+  price: string | null;
+};
 export const fetchProduct = async () => {
   try {
     const products = await db.product.findMany();
-    products.forEach((product) => {
+    products.forEach((product: ProductType) => {
       if (product.price !== null) {
         product.price = formatToString(product.price);
       }
